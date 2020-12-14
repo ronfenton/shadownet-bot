dee053d4ab4f40908d65c6ed61bdb4cfbc858ce0
DingoSoulEater <fentonron@live.com.au>
2020-12-14 19:58:35 +0100

    Basic structure added

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 mongoose.connect(process.env.DB_ADDRESS,{useNewUrlParser:true,useUnifiedTopology:true});
 
-const serverSchema = new mongoose.Schema ({serverID:"String"});
-const userSchema = new mongoose.Schema ({discord_id:"String"});
-const charSchema = new mongoose.Schema ({
-    name: "String",
-    dr: drSchema,
-    def: "Number",
-    initiative: "String",
-    discord_id: "String",
-    note: "String"
-});
-const drSchema = new mongoose.model({
-    armour: "Number",
-    bonus : "Number",
-    hard  : "Number",
-    body  : "Number"
+const characterSchema = new mongoose.Schema({
+    name: String,
+    def: {type: Number,default:0},
+    flags: {type: [String],default:[]},
+    initiative: {type: String,default:"1d6+1"},
+    note: {type: String,default:""},
+    owner_id:String
+})
+const playerSchema = new mongoose.Schema({
+    characters: [String],
+    discord_id: String,
+    active: String,
+    friendly_name: String,
+    rollstats: {type: Object,default: {1:0,2:0,3:0,4:0,5:0,6:0}}
+})
+const serverSchema = new mongoose.Schema({
+    gms: {type: [String],default:[]},
+    players: {type: [playerSchema],default: []},
+    npcs: {type: [characterSchema],default: []},
+    characters: {type: [characterSchema],default: []},
+    npcrole: String,
+    _id: String,
+    name: String
 })
 
+const Character = mongoose.model("Character",characterSchema);
+const Player = mongoose.model("Player",playerSchema);
 const Server = mongoose.model("Server",serverSchema);
-const User = mongoose.model("User",userSchema);
-const Char = mongoose.model("Char",charSchema);
-const DR = mongoose.model("DRVal",drSchema);
 
 client.once('ready',() => {
     console.log('Ready!')
+    console.log("Current Guilds:");
+    let guildArray = client.guilds;
+    guildArray.cache.forEach(function(element){
+        console.log("Member of "+element.name+" (id: "+element.id+")");
+    });
+    
 });
 
 client.login(process.env.DISCORD_TOKEN)
@@ -39,38 +53,163 @@ client.on('message',message => {
     // if message originated from a bot; ignore it.
     if(message.author.bot){ return; }
     
-    // identify if there's a command.
-    let command = message.content.match(/\(([^)]+)\)/);
-    if(command){
-        console.log(command);
-        shadowBot(command[1],message.channel,message.author);
-    } 
+    let input = message.content.match(/(?<=\()(?<actor>[\w\s]+(?=!))?!?(?<command>set|def|resist|init|info|[\d]+[!egw]*)(?:\s(?<args>[^\)]+))?(?=\))/i);
+    if(input === null) { return; }
+    else {input = input.groups}
+    // note; input.groups.actor = actor replacement, undefined if missing. command = command type. args = remaining string.
+
+    Server.findById(message.guild.id,function(err,server){
+        if(!err){
+            if(server === null){
+                console.log("Server not already logged; new reference made.")
+                // if server not in mongoose, make new.
+                server = new Server({
+                    gms:[],
+                    players:[],
+                    npcs:[],
+                    characters:[],
+                    npcrole:"",
+                    teamrole:"",
+                    _id:message.guild.id,
+                    name:message.guild.name
+                })
+            } 
+
+            // return the player that made the message request.
+            let player = server.players.filter(item => item.discord_id === message.member.id)[0];
+            if(!player){
+
+                console.log("Player not already logged; new reference made.")
+                // if no player in server log; add new.
+                player = new Player({
+                    discord_id: message.author.id,
+                    active: null,
+                    friendly_name: message.member.displayName,
+                    characters: [],
+                })
+                server.players.push(player);
+            }
+
+            handleTask(server,player,input);
+            
+            server.save();
+
+        } else {
+            throw new Error(err);
+        }
+    }) 
     
 })
 
-function shadowBot(input,channel,user){
-    
-    if(/^\d/.test(input)){
-        // if it's a number, do stuff here.
-        let ndie = Number(input.match(/\d+/g));
-        let nhits = 0;
-        let nsequence = [];
-        for(let i = 0; i < ndie; i++){
-            let roll = Math.ceil(Math.random()*6);
-            if(roll >= 5){
-                nhits++;
-            }
-            nsequence.push(roll);
-        }
-        channel.send(`<@${user.id}> You got **${nhits}** hits. \`[${nsequence.join(", ")}]\``);
-    } else {
-        let inst = input.toUpperCase().match(/^(\w{1,5}[-:=+]?)\s?([-+\d\w\s]+)?/);
-        if(inst){
-            let task = inst[1];
-            let args = inst[2];
-            ((args === undefined) ? args = [] : args = args.split(" "));
-            // handle here.
+function handleTask(server,player,request){
 
+    let command = request.command;
+    let args = request.args;
+    let actor;
+    if(request.actor){
+        // create or use existing actor / NPC 
+    } else if (player.active) {
+        // use player's active character
+    } 
+
+    // TEMPORARY ACTOR FOR TESTING / DEVELOPMENT.
+    actor = new Character({name:"test",owner_id:"abcdefghijklmnop"})
+
+    let diceTest = command.match(/^(?<die>[\d]+)(?<args>[egw!]+)?/);
+
+    if(diceTest){
+        if(diceTest.groups.args) {
+            args = arrToArgs(diceTest.groups.args.split(""));
+        }
+        console.log(srRoll(Number(diceTest.groups.die),player,args));
+    } else {
+        if(args) { args = arrToArgs(args.split(/\s/))}
+        switch(command){
+            
         }
     }
+
+}
+
+/* 
+    Rolls nDie d6's and records the amount of hits (5 & 6),
+    and glitches (1). Accepts arguments to adjust logic.
+    args:
+        player: provide a Player object to have die rolls
+        contribute to recording of rolls for stats. 
+        explode: makes 6's reroll for additional die.
+    Returns an object as follows:
+    { hit: Number, glitch: Number, rollsets: Array of Rollgroups, rolled: Number }
+*/
+function srRoll(nDie,player,args){
+
+    if(!args){ args = {} }
+    
+    function roller(dice,reroll,player,counter){
+
+        // if counter inexistent; make new one.
+        if(!counter){
+            counter = {hit:0, glitch:0, rollsets:[], rolled: 0}
+        }
+
+        // counter for second-pass dice.
+        let moreDie = 0; rolled = [];
+        
+        // generate rolls equal to **dice**
+        for(let x = 0; x < dice; x++){
+            let roll = Math.ceil(Math.random()*6);
+            if(roll>=5){ counter.hit++; }
+            if(roll>=6){ moreDie++; }
+            if(roll === 1){ counter.glitch++; }
+            counter.rolled++;
+            if(player) {player.rollstats[roll]++;}
+            rolled.push(roll);
+        }
+
+        // push the roll set
+        counter.rollsets.push(rolled);
+        
+        // if it was an edge roll; roll again with any extra die.
+        if(reroll && moreDie > 0){
+            nextRoll = roller(moreDie,reroll,player,counter);
+        }
+
+        return counter;
+    }
+
+    return roller(nDie,args["!"],player)
+}
+
+function arrToArgs(arr){
+    let obj = {};
+    arr.forEach(function(element){
+        let match = element.match(/^(?<name>[A-Za-z!]+)?(?<val>[-+\dd]+)?$/)
+        if(match){
+            let propname = match.groups.name;
+            let propval = match.groups.val;
+
+            // if no property name provided; assume it's the default case.
+            if(!propname){
+                propname = "default";
+            }
+
+            // if no value provided, assume it's a true flag.
+            if(!propval){
+                propval = true;
+            }
+
+            // if propval = -, set to 'false'.
+            if(propval === "-"){
+                propval = false;
+            }
+
+            // determine if value is a number.
+            if(/\+?[-\d]+/.test(propval)){
+                propval = Number(propval.match(/[-\d]+/));
+            }
+            obj[propname] = propval;
+        }
+    })
+    console.log(obj);
+    return obj;
 }
--- readme.txt
+++ readme.txt
@@ -1,27 +1,75 @@
-So been doing a little testing with this Bot and it's surprisingly straightforward (which of course means I'll make a fuckton of goofs from overconfidence). So I set out a simple plan for it as follows;
+All commands are to be surrounded in parenthesis; these can be anywhere in any discord post. Anything in bolded brackets is for user entry. eg.
+(set name <Character Name>) means to set your name to Bob, type (set name Bob). # is for inputting numbers.
+Capitalisation is not important. It is ignored for anything that isn't a text entry (names and notes)
 
-Planned design.
 
-For all commands, must be flanked by ().
+// SIMPLE TESTS //
+Direct Checks / Tests
+(#) Roll a simple test.
+(#!) Roll a simple test with Exploding Sixes (Push the Limit)
+(#e) Roll an extended test; after each roll, react to the message to roll again with -1 die and counting hits.
+Note- you can append 'g' to any of these, to indicate that the character's Gremlins Rating (flag gremin#) applies; and w if your wound modifier applies.
 
-# = roll # die
-#! = roll # exploding die
-#e = Start extended test.
+// SETTING UP THE BOT //
+Character Settings
+(char name <Name>) sets the character's name. (set name Wheaton)
+(char init <Initiative Die with Adds> <Meat, Astral, or Matrix>) sets the character's initiative die, and what type. If type is ommitted, defaults to Meatspace. (set i 3d6+9) or (set i 5d6+12 Matrix)
+(char armour <any of Arm#, Hard#, Temp#>) sets the character's armour. If keyword omitted, assumes armour. (set armour 10) or (set armour hard25)
+(char soak <any of Bod#, Temp#>) sets the characters body soak. If keyword omitted, assumes body. (set soak 7) or (set soak temp2)
+(char def #) sets the character's defence pool against attacks. (set def 12)
+(char note <Any Text>) updates a character note; this will appear during your initiative call-outs for reminders.
+(char change <Any Character Name>) changes to a different saved character. If you try set this to a character that isn't recorded, this will ask if you wish to make a new one. (set character Bones)
+(char flag <Any Flag>) adds a flag. Postpend the flag name with a dash (-) to remove it instead. (set flag spirit) or (set flag simhot-). Flags will affect secondary systems.
+(char woundpen #) sets your current wound penalty; automatically affects initiative.
+(char info) presents a section of character info.
+(char karma #) adjusts karma +/-
+(char cred #) adjusts street-cred +/-
+(char notoriety #) adjusts notoriety +/-
+(char nuyen #) adjusts nuyen +/-
+(char transfer <PlayerName>) transfers a character to another player.
 
-command arg arg arg arg = perform command with [args]. *not order specific*
-each arg will either be text or text#.
+Setting up your Player (A lot of this is default)
+(player name <Any Player Name>) changes your friendly player name.
+(player info) Will present a set of information specific to your player account.
+(player flag <Any Flag>) adds a flag. Prepend the flag name with a dash (-) to remove it instead.
 
-Anything in [] is *optional* flags for added function.
+// USING THE BOT //
+Combat
+(def <#> <vs#>) performs a defense test, with a given modifier. If provided, the vs# indicates how many hits your opponent rolled, to auto-determine pass/fail.
+(resist <ap#> <dmg#>) performs a standard damage resistance test, automatically determining Physical to Stun conversion.
+(init join <Meat, Astral, or Matrix> <#> <pass?>) Joins the fight at a given initiative. If type omitted, meatspace assumed. Optional provision of flat +/- mod.
+(init <-#, +#, or =#>) Modifies existing initiative score.
+(init remove) Removes the given character from the combat chart.
 
-i= #d#+# [Matrix/Astral] = Set and roll initiative to #d#+#. Flags will save alternate ini scores.
-i [Matrix/Astral] = Roll initiative. Flag will roll alternate saved ini score.
-i+ # = modify initiative.
-i- # = minus initiative.
-iset # = Sets initiative to a specific number. Not set to = because it's going to be used far less than just setting ini die.
-dr= # [Bod#, Bonus#, Hard#] = Set DR, optional flags for Body, Hardened, and Bonus.
-dr [#P or #S, AP-#] = Roll DR test against that damage.
-def= # = Set defense die.
-def [#, #P/#S] = Roll defense die. If damage provided, automatically work out net hits + damage if failed.
-note = Accepts no args, sets note to remainder of string.
+// GM SPECIFIC//
+NPC Actors
+(<Name>!<any other command>) Perform any action as any other character; if character doesn't exist, one is created as NPC. Note; when performed with the set init command, it automatically joins combat if ongoing.
+(<Name>!remove) Removes the NPC.
+(<Name>!promote) Changes an NPC into a DM-Owned PC.
+(<Name>!demote) Changes a PC into an NPC.
 
-!<string>:<command> = Perform above for any NPC agent. EG, (!Harriet Jones:5) will roll 5 die for Harriet Jones. (!Jack:def 24) will roll Jack's defense die and indicate net hits against 24.
+Team Management
+(team join <Meat, Astral, or Matrix>) All players join the fight using either the selected Initiative (if listed), or Meatspace Initiative.
+(team info) Indicates the current team.
+(team role <@mention>) Sets the team's Discord role.
+
+Server Settings
+GM Only
+(server info) Presents a set of info about the server, such as NPCs, Characters, etc.
+(set gmrole <@mention>) Updates the gmrole for NPC Initiative pings.
+(init start) Starts a new Combat Turn with all current NPC Actors.
+(init stop) Clears combat status entirely, also terminates any 'Temp' mods.
+
+Flags and Effects
+nostun - Appropriate for Vehicles and Drones; prevents stun damage entirely.
+gremlin# - Sets a Gremlin variable for the character. Any die roll appended with g will be subjected to it.
+friendly - for GM characters; has them show up in Team initiative.
+
+
+expected characters;
+\w (a-zA-Z0-9)
+\s (whitespace)
+! (for NPC commands and edge)
++ - = (for math commands; always preceeding \d)
+@ (from mentions)
+. (for notes only)
